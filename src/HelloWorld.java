b1e3993b4daee663df523999cae5f271bc1288ea
Jean-Philippe Ehret <jehret@jnesis.com>
2021-02-03 18:50:46 +1300

    2500_OPERATEURS_LOGIQUES

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String... args){
         boolean bool1=true;
         boolean bool2=false;
-        boolean estCeQueCestVrai=bool1!=bool2;
+        boolean estCeQueCestVrai=bool1||bool2;
         System.out.println(estCeQueCestVrai);
     }
 }
