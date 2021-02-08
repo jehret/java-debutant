5599b72d613f91b410c12d38eafab9853f160a47
Jean-Philippe Ehret <jehret@jnesis.com>
2021-02-08 14:29:42 +1300

    2950_PRINTF

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
     public static void main(String... args){
-        String debut="Mon age est ";
         int age=43;
-        System.out.println(debut+(age+1));
+        System.out.printf("Mon age est %d ans. Oui, je fais plus jeune.",age);
     }
 }
