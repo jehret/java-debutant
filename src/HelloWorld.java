e968499911c3f23fbcad4fc51c8b411fcfd404f8
Jean-Philippe Ehret <jehret@jnesis.com>
2021-02-11 18:43:45 +1300

    3000_BLOC_INSTRUCTIONS

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,15 @@
 public class HelloWorld {
-    public static void main(String... args){
-        int age=43;
-        System.out.printf("Mon age est %d ans. Oui, je fais plus jeune.",age);
+    public static void main(String... args) {
+        int age = 43;
+        System.out.printf("Mon age est %d ans. Oui, je fais plus jeune.", age);
+
+        int ageDuCapitaine=38;
+        {
+            System.out.println("L'age du capitaine est "+ageDuCapitaine);
+            System.out.println("Hello");
+            System.out.println("World!");
+        }
+
+
     }
 }
