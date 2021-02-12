917bfd392d9152c77d89d5a909ec98b9cc323282
Jean-Philippe Ehret <jehret@jnesis.com>
2021-02-12 20:16:45 +1300

    3100_IF

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,13 @@
 public class HelloWorld {
     public static void main(String... args) {
         int age = 43;
-        System.out.printf("Mon age est %d ans. Oui, je fais plus jeune.", age);
+        System.out.printf("Mon age est %d ans.", age);
+        if (age > 30) {
+            System.out.println(" Oui, je fais plus jeune.");
+        }
 
-        int ageDuCapitaine=38;
+        int ageDuCapitaine=20;
+        if (ageDuCapitaine > 20 && age < 50)
         {
             System.out.println("L'age du capitaine est "+ageDuCapitaine);
             System.out.println("Hello");
