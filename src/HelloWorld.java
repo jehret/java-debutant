f20cf534e2cc11ffd5cb3fddda6e3592925688c7
Jean-Philippe Ehret <jehret@jnesis.com>
2021-03-03 16:52:28 +1300

    3350_OPERATEUR_TERNAIRE

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,18 +1,14 @@
 public class HelloWorld {
     public static void main(String... args) {
-        int age = 50;
+        int age = 45;
         System.out.printf("Mon age est %d ans.", age);
         if (age >= 30) {
             System.out.println(" Oui, je fais plus jeune.");
         }
 
         int ageDuCapitaine=25;
-        if (ageDuCapitaine > 20 && age < 50) {
-            System.out.println("L'age du capitaine est "+ageDuCapitaine);
-        }
-        else {
-            System.out.println("Le code ne permet pas d'afficher l'âge du capitaine pour une raison farfelue.");
-        }
+
+        System.out.println(ageDuCapitaine > 20 && age < 50 ? "L'age du capitaine est "+ageDuCapitaine : "Le code ne permet pas d'afficher l'âge du capitaine pour une raison farfelue.");
 
     }
 }
