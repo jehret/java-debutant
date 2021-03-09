9631080cb154156c1cf4e8c9df6fa41fb0e9e25e
Jean-Philippe Ehret <jehret@jnesis.com>
2021-03-10 11:53:04 +1300

    3500_IF_ELSE_IF_IF

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,19 @@
 public class HelloWorld {
     public static void main(String... args) {
-        int age = 45;
+        int age = 23;
         System.out.printf("Mon age est %d ans.", age);
         if (age >= 30) {
             System.out.println(" Oui, je fais plus jeune.");
         }
-
-        int ageDuCapitaine=25;
-
-        System.out.println(ageDuCapitaine > 20 && age < 50 ? "L'age du capitaine est "+ageDuCapitaine : "Le code ne permet pas d'afficher l'âge du capitaine pour une raison farfelue.");
+        else if (age < 10){
+            System.out.println(" Bravo à toi de commencer si jeune !");
+        }
+        else if (age == 25){
+            System.out.println(" 25 ans vous devez être en pleine forme !");
+        }
+        else {
+            System.out.println(" Et je suis fier de mon âge.");
+        }
 
     }
 }
