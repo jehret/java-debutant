5652c8db76070c76c0577d855d5c718b7bea24b6
Jean-Philippe Ehret <jehret@jnesis.com>
2021-03-19 17:09:57 +1300

    3900_FOR

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,29 +1,8 @@
 public class HelloWorld {
     public static void main(String... args) {
-        String mois="Mai";
 
-        switch (mois) {
-            case "Janvier" :
-            case "Février" :
-            case "Mars" :
-                System.out.println("C'est l'hiver");
-                break;
-            case "Avril" :
-            case "Mai" :
-            case "Juin" :
-                System.out.println("C'est le printemps");
-                break;
-            case "Juillet" :
-            case "Août" :
-            case "Septembre" :
-                System.out.println("C'est l'été");
-                break;
-            case "Octobre" :
-            case "Novembre" :
-            case "Décembre" :
-                System.out.println("C'est l'automne");
-                break;
-
+        for (int nb=0 ;nb<3; nb++){
+            System.out.println("Hello World!");
         }
 
     }
