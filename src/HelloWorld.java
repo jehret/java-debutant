4310330d923b965653a2a274164be47482c6c250
Jean-Philippe Ehret <jehret@jnesis.com>
2021-03-19 16:08:03 +1300

    3700_SWITCH

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,18 +1,29 @@
 public class HelloWorld {
     public static void main(String... args) {
-        int age = 23;
-        System.out.printf("Mon age est %d ans.", age);
-        if (age >= 30) {
-            System.out.println(" Oui, je fais plus jeune.");
-        }
-        else if (age < 10){
-            System.out.println(" Bravo à toi de commencer si jeune !");
-        }
-        else if (age == 25){
-            System.out.println(" 25 ans vous devez être en pleine forme !");
-        }
-        else {
-            System.out.println(" Et je suis fier de mon âge.");
+        String mois="Mai";
+
+        switch (mois) {
+            case "Janvier" :
+            case "Février" :
+            case "Mars" :
+                System.out.println("C'est l'hiver");
+                break;
+            case "Avril" :
+            case "Mai" :
+            case "Juin" :
+                System.out.println("C'est le printemps");
+                break;
+            case "Juillet" :
+            case "Août" :
+            case "Septembre" :
+                System.out.println("C'est l'été");
+                break;
+            case "Octobre" :
+            case "Novembre" :
+            case "Décembre" :
+                System.out.println("C'est l'automne");
+                break;
+
         }
 
     }
