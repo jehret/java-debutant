74e0735ec9db5dbe4846bc65b4ded91d77bd9abb
Jean-Philippe Ehret <jehret@jnesis.com>
2021-03-19 17:24:28 +1300

    3900_FOR fix2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
     public static void main(String... args) {
-        int nb=0;
+        int nb=0 ;
         for (;nb<3; nb++){
             System.out.println("Hello World!");
         }
-
+        System.out.println("Nb vaut maintenant "+nb);
     }
 }
