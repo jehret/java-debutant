a5384dcaeb12a619949b31797fa124038f47a97d
Jean-Philippe Ehret <jehret@jnesis.com>
2021-03-19 18:15:21 +1300

    4300_BREAK_CONTINUE

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,18 @@
 public class HelloWorld {
     public static void main(String... args) {
-        boolean jeContinue=false;
-        do {
+        boolean jeContinue=true;
+        int nb=0;
+        while (jeContinue) {
             System.out.println("Hello World!");
-            jeContinue=true;
+            nb++;
+            if (nb==5){
+                continue;
+            }
+            System.out.println("Nb ne vaut pas 5");
+            if (nb==10){
+                jeContinue=false;
+            }
         }
-        while (jeContinue);
+
     }
 }
