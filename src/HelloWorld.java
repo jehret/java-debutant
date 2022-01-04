36e1c8bb26984e05450cfe2036c965c6e829b2c9
Jean-Philippe Ehret <jehret@jnesis.com>
2022-01-04 18:03:04 +1300

    5000_MODIFIER_ATTRIBUT

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,20 +2,11 @@ public class HelloWorld {
     public static void main(String... args) {
 
         Voiture voitureDeMichel=new Voiture();
+        voitureDeMichel.nbPortes=3;
+        voitureDeMichel.automatique=true;
+        voitureDeMichel.couleur="Vert";
 
-        boolean jeContinue=true;
-        int nb=0;
-        while (jeContinue) {
-            System.out.println("Hello World!");
-            nb++;
-            if (nb==5){
-                continue;
-            }
-            System.out.println("Nb ne vaut pas 5");
-            if (nb==10){
-                jeContinue=false;
-            }
-        }
+        System.out.println("La voiture est "+voitureDeMichel.couleur+" et elle dispose de "+voitureDeMichel.nbPortes+" portes.");
 
     }
 }
